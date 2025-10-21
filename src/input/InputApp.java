120048ac2f707fbc599c994855c436c1534de838
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2025-10-21 15:53:12 -0400

    InputApp: change input structure to top-level KeyListener

--- src/input/InputApp.java
+++ src/input/InputApp.java
@@ -2,15 +2,21 @@ package input;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class InputApp extends JFrame {
+public class InputApp extends JFrame implements KeyListener {
+    private static final InputCanvas inputCanvas = new InputCanvas();
+
     public static void main(String[] args) {
-        new InputApp();
+        new InputApp(inputCanvas);
     }
 
-    public InputApp() {
+    public InputApp(InputCanvas canvas) {
         this.setLayout(new BorderLayout());
-        this.add(new InputCanvas(), BorderLayout.CENTER);
+        this.add(canvas, BorderLayout.CENTER);
+
+        this.addKeyListener(this);
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(720, 480);
@@ -27,5 +33,32 @@ public class InputApp extends JFrame {
         int adjY = centerY - mySize.height / 2;
 
         this.setLocation(adjX, adjY);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        System.out.printf("Key Typed: %c%n", e.getKeyChar());
+
+        inputCanvas.setBgColor(switch (e.getKeyChar()) {
+            case 'r' -> Color.RED;
+            case 'a' -> Color.ORANGE;
+            case 'i' -> Color.YELLOW;
+            case 'n' -> Color.GREEN;
+            case 'b' -> Color.CYAN;
+            case 'o' -> Color.BLUE;
+            case 'w' -> Color.MAGENTA;
+
+            default -> Color.BLACK;
+        });
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
--- src/input/InputCanvas.java
+++ src/input/InputCanvas.java
@@ -2,39 +2,13 @@ package input;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 
 public class InputCanvas extends JPanel {
-    private Color bgColor;
+    private Color bgColor = Color.WHITE;
 
-    public InputCanvas() {
-        bgColor = Color.WHITE;
-
-        InputMap inputMap = this.getInputMap();
-        ActionMap actionMap = this.getActionMap();
-
-        inputMap.put(KeyStroke.getKeyStroke("R"), "red");
-        actionMap.put("red", new ColorAction(Color.RED));
-
-        inputMap.put(KeyStroke.getKeyStroke("A"), "orange");
-        actionMap.put("orange", new ColorAction(Color.ORANGE));
-
-        inputMap.put(KeyStroke.getKeyStroke("I"), "yellow");
-        actionMap.put("yellow", new ColorAction(Color.YELLOW));
-
-        inputMap.put(KeyStroke.getKeyStroke("N"), "green");
-        actionMap.put("green", new ColorAction(Color.GREEN));
-
-        inputMap.put(KeyStroke.getKeyStroke("B"), "cyan");
-        actionMap.put("cyan", new ColorAction(Color.CYAN));
-
-        inputMap.put(KeyStroke.getKeyStroke("O"), "blue");
-        actionMap.put("blue", new ColorAction(Color.BLUE));
-
-        inputMap.put(KeyStroke.getKeyStroke("W"), "pink");
-        actionMap.put("pink", new ColorAction(Color.PINK));
+    public void setBgColor(Color bgColor) {
+        this.bgColor = bgColor;
     }
-
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -45,19 +19,5 @@ public class InputCanvas extends JPanel {
         g2.fillRect(0, 0, windowSize.width, windowSize.height);
 
         repaint();
-    }
-
-
-    class ColorAction extends AbstractAction {
-        private final Color color;
-
-        public ColorAction(Color color) {
-            this.color = color;
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent ignored) {
-            bgColor = color;
-        }
     }
 }
